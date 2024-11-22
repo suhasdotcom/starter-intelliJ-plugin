7f9a64986dfce87e221538b0eaef84fb4cbdb9e5
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2024-11-22 14:20:01 +0530

    encGitVcs added.

---
+++ src/main/kotlin/com/github/suhasdotcom/enc/EncGitVcs.kt
@@ -0,0 +1,31 @@
+package com.github.suhasdotcom.enc
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.AbstractVcs
+import com.intellij.openapi.vcs.VcsKey
+import com.intellij.openapi.vcs.VcsType
+import git4idea.GitVcs
+
+class EncGitVcs(project: Project) : AbstractVcs(project, ID) {
+
+    companion object {
+        const val ID = "EncGit" // Unique ID for your VCS
+        fun getKey(): VcsKey = createKey("Enc");
+    }
+    private val gitVcs: GitVcs = GitVcs(project)
+    override fun getDisplayName(): String = "Enc"
+
+    // Delegate overridden methods to gitVcs
+    override fun getCheckinEnvironment() = gitVcs.checkinEnvironment
+    override fun getDiffProvider() = gitVcs.diffProvider
+    override fun getMergeProvider() = gitVcs.mergeProvider
+    override fun getAnnotationProvider() = gitVcs.annotationProvider
+    override fun getCommittedChangesProvider() = gitVcs.committedChangesProvider
+    override fun getVcsHistoryProvider() = gitVcs.vcsHistoryProvider
+    override fun getUpdateEnvironment() = gitVcs.updateEnvironment
+    override fun getStatusEnvironment() = gitVcs.statusEnvironment
+
+    override fun getType(): VcsType = gitVcs.type
+
+    // Delegate any other methods as required
+}
